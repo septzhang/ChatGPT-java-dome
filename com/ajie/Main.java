ee5893f952f019990fb56abbf8d7f886dc25ac5c
septzhang_mac <zhangweijie1024@qq.com>
2023-03-21 10:06:55 +0800

    项目构建

---
+++ com/ajie/Main.java
@@ -0,0 +1,13 @@
+package com.ajie;
+
+/**
+ * @author septzhang
+ * @version 1.0
+ * @email zhangweijie1024@qq.com
+ * @date 2023/3/21 09:56
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("nihao");
+	}
+}
